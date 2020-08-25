9b8ff018e1cdb5eca12a24342c7d933f886b0e63
Gustavo Duarte <gduartemenezes@gmail.com>
2020-08-25 20:03:45 -0300

    Add Migration: create-reports

---
+++ src/database/migrations/20200825221449-create-reports.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('reports', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      table: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      column: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      parameters: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('reports');
+  },
+};
